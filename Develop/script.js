09f84edfd03a98adf22c1c90a3a7a23521ffeb6c
Macheya Steele <mcheysteele@gmail.com>
2020-11-13 15:50:02 -0700

    starter code

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,45 @@
-// Assignment code here
+// USER INPUT VARIABLES
+
+//ALPHABET VALUES
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// NUM VALUES
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// SPECIAL CHAR VALUES
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//GENERATE RANDOM PASSWORD
+function generatePassword() {
 
-  passwordText.value = password;
+
+//SET PW LENGTH
+var pwLength = window.promt("How many characters would you like your password to be? Please choose between 8-128 characters.");
+
+
 
 }
 
+
+
+
+
+
+
+
+//INSTRUCTION:
+
+// Get references to the #generate element
+//var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+
+ // passwordText.value = password;
+
+//}
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
