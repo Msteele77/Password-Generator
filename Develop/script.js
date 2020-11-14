3da13662038dabdd3fa07d01d87273d96e80fcd8
Macheya Steele <mcheysteele@gmail.com>
2020-11-13 19:24:15 -0700

    defined variables, added input character type prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,45 +1,101 @@
-// USER INPUT VARIABLES
+//POSSIBLE PW VALUES:
 
-//ALPHABET VALUES
-alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// NUM VALUES
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// SPECIAL CHAR VALUES
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "1234567890";
+var character = "!@#$^&%*()+=-[]{}|:<>?,.";
 
-
-
-//GENERATE RANDOM PASSWORD
-function generatePassword() {
+var pwd = '';
 
 
-//SET PW LENGTH
-var pwLength = window.promt("How many characters would you like your password to be? Please choose between 8-128 characters.");
+var lowerSelection = false;
+var upperSelection = false;
+var numberSelection = false;
+var characterSelection = false;
 
 
 
+
+//GENERATE PW FUNCTION
+function generate() {
+  var confirmLength = "";
+  //LENGTH INPUT FROM USER
+  while (isNaN(confirmLength) || confirmLength < 8 || confirmLength > 128) {
+    confirmLength = prompt("The length of your random password can be between 8-128 characters. Please type your answer below to select the length.");
+  if (confirmLength === null) {
+  break;
+    }
+  }
+
+//CHARACTER TYPE INPUT FROM USER
+  if (confirmLength) {
+
+  if (confirm("Would you like to use lowercase characters in your strong password?") == true) {
+      lowerSelection = true
+  }   
+
+  if (confirm("Would you like to use uppercase characters in your strong password?") == true) {
+    upperSelection = true
+  }
+
+  if (confirm("Would you like to use special characters in your strong password?") == true) {
+    characterSelection = true
+  }
+
+  if (confirm("Would you like to use numerical characters in your strong password?") == true) {
+    numberSelection = true
+  }
+
+//NO CHAR TYPE SELECTED
+  if (lowerSelection === false && upperSelection === false && characterSelection === false && numberSelection === false) {
+    alert("You must choose at least one character type to generate a strong password.")
+    }
+  }
+
+//PW GENERATION
+var characters = '';
+    characters += (lowerSelection ? lower : '');
+    characters += (upperSelection ? upper : '');
+    characters += (characterSelection ? character : '');
+    characters += (numberSelection ? numbers : '');
+
+    pwd = password(confirmLength, characters);
+
+    document.getElementById("password").innerHTML = pwd;
+}
+
+function password(l, characters) {
+  var pwd = '';
+  for (var i = 0; i < l; ++i) {
+      pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return pwd;
 }
 
 
 
 
+  
 
 
 
 
-//INSTRUCTION:
 
-// Get references to the #generate element
-//var generateBtn = document.querySelector("#generate");
+
+
+
+
+
+
+
+/*
+var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
 //function writePassword() {
   //var password = generatePassword();
   //var passwordText = document.querySelector("#password");
 
- // passwordText.value = password;
-
-//}
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+ // passwordText.value = password*/
